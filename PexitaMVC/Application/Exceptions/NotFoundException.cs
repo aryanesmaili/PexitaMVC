3ff11fbde8e0d9d7b8efd60849529458d423d3d0
Aryan <aryanesmailiea@gmail.com>
2024-12-02 05:05:51 +0330

    new exception added to address situations where a data is not found

---
+++ PexitaMVC/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace PexitaMVC.Application.Exceptions
+{
+    [Serializable]
+    internal class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
